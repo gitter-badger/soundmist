7377b5735b9525da8d2d6e53ae17a2204fab3c6a
Leo Sandström <leosand97@gmail.com>
2016-06-23 21:34:43 +0200

    Add npm scripts for running Gulp's preinit and starting Electron

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,36 @@ var gulp = require('gulp'),
     livereload = require('gulp-livereload'),
     jade = require('gulp-jade');
 
-let structured = {base: '.'};
+let out = './dist'
+let structured = {
+  base: '.'
+};
 
-gulp.task('jade', function() {
-  gulp.src('./**/*.jade')
+gulp.task('vital', () => {
+  gulp.src('package.json').pipe(gulp.dest(out))
+})
+
+gulp.task('jade', () => {
+  gulp.src('./**/*.jade', structured)
     .pipe(jade())
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest(out))
     .pipe(livereload());
 })
 
-gulp.task('scripts', function() {
+gulp.task('scripts', () => {
   gulp.src(['main.js', 'scripts/**/*.js'], structured)
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest(out))
     .pipe(livereload())
 })
 
-gulp.task('watch', function() {
+gulp.task('watch', () => {
   livereload.listen();
+
+  gulp.watch('package.json', ['vital'])
   gulp.watch('./**/*.jade', ['jade']);
   gulp.watch(['main.js', 'scripts/**/*.js'], ['scripts']);
 });
+
+gulp.task('start', () => {
+  gulp.start('vital', 'jade', 'scripts');
+})
