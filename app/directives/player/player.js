c1c34b9db2a9fce148afff82bb4fdaf8e1377fe0
Leo Sandström <leosand97@gmail.com>
2016-07-01 22:32:15 +0200

    Add slider to header for indicating and changing track progress

--- app/directives/player/player.js
+++ app/directives/player/player.js
@@ -4,8 +4,10 @@ angular.module('soundmist').directive('player', function (Player) {
     scope: false,
     replace: false,
     templateUrl: 'directives/player/player.html',
-    link: function (scope) {
+    link: function (scope, element) {
       scope.Player = Player
+      let slider = element.find('md-slider')[0]
+      let blocking = false
 
       scope.$watch('Player.getActive()', function (item) {
         if (item == undefined) return
@@ -15,6 +17,25 @@ angular.module('soundmist').directive('player', function (Player) {
         var url = item.track.artwork_url.replace('large.jpg', 't500x500.jpg')
         scope.wallpaper = {
           'background': 'linear-gradient(rgba(255, 126, 0, 0.90),rgba(255, 119, 0, 0.75)), url(' + url + ')'
+        }
+      })
+
+      scope.$watch('Player.getProgress(item)', function (progress) {
+        if (!blocking) scope.progress = progress * 1000
+      })
+
+      slider.addEventListener('mousedown', event => {
+        blocking = true
+      })
+
+      slider.addEventListener('mouseup', function (event) {
+        if (blocking) {
+          let x = event.pageX - this.offsetLeft
+          let progress = x / slider.offsetWidth
+
+          Player.setProgress(scope.item, progress)
+
+          blocking = false
         }
       })
     }
